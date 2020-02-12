10175386c5d0f195d8f85960720340566d1ef04e
timislearning <tim.mitzen@gmail.com>
2020-02-12 08:55:36 -0800

    api post

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+const express = require('express');
+
+const users = require('./data/seeds/users.js');
+
+const server = express()
+server.get('/',(req,res)=>{
+   res.json({ message: 'This is the server'})
+})
+
+server.post('/api/users', (req,res) => {
+   const newUser = {
+      id: users.length +1, //adds 1 to the user id
+      name: req.body.name,
+      bio: req.body.bio,
+   }
+   
+   users.push(newUser);//pushs new user to the users object
+   res.statusMessage(201).json(newUser); //saying its completed
+
+});
+
+// server.get('/api/users/:id', (req,res)=>{
+//    findById()
+//    .then()
+//    .catch()
+// });
+const port = 8000
+
+server.listen(port,()=>{
+   console.log(`server has started on ${port}`)
+});
+
